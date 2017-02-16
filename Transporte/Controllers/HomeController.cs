a64fdcf61bb57369068af46c6c7afd66ecad34f1
EbarriosCode <guayoswing@gmail.com>
2017-02-16 09:38:37 -0600

    Archivos Base del proyecto en este primer commit se agrega Lá pagina web Estática FrontEnd

---
+++ Transporte/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Transporte.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProgramacionViajes()
+        {
+            return View();
+        }
+
+        public ActionResult TerminosCondiciones()
+        {
+            return View();
+        }
+
+        public ActionResult AntesDeViajar()
+        {
+            return View();
+        }
+
+        public ActionResult Contacto()
+        {
+            return View();
+        }
+
+        public ActionResult AcercaDe()
+        {
+            return View();
+        }
+    }
+}
